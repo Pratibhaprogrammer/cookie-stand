3171ab86386e6ecf91003b84f466c616044644de
Jacob Holmer <yacub90@gmail.com>
2021-01-19 20:11:34 -0800

    updated page2

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ var Seattle = {
   minCust: 23,
   maxCust: 65,
   cookieSale: 6.3,
+  cookieCount: 0,
   numOfCustomers: function (){
     var randomCustomers = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
     return randomCustomers
+
     
   },
   cookiesPerHour: [],
@@ -20,6 +22,7 @@ var Tokyo ={
   minCust: 3,
   maxCust: 24,
   cookieSale: 1.2,
+  cookieCount: 0,
   numOfCustomers: function (){
     var randomCustomers = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
     return randomCustomers
@@ -35,6 +38,7 @@ var Dubai = {
   minCust: 11,
   maxCust: 38,
   cookieSale: 3.7,
+  cookieCount: 0,
   numOfCustomers: function (){
     var randomCustomers = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
     return randomCustomers
@@ -48,6 +52,7 @@ var Paris = {
   minCust: 20,
   maxCust: 38,
   cookieSale: 2.3,
+  cookieCount: 0,
   numOfCustomers: function (){
     var randomCustomers = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
     return randomCustomers
@@ -62,6 +67,7 @@ var Lima ={
   minCust: 2,
   maxCust: 16,
   cookieSale: 5.6,
+  cookieCount: 0,
   numOfCustomers: function (){
     var randomCustomers = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
     return randomCustomers
@@ -72,21 +78,36 @@ var Lima ={
 
 var locations = [Seattle, Tokyo, Dubai, Paris, Lima]
 for(var j = 0; j < locations.length; j++){
+  
   for(var i = 0; i < hoursOfOperation.length; i++){
     var customers = locations[j].numOfCustomers();
     var cookiesThisHour = Math.floor(customers * locations[j].cookieSale)
+    locations[j].cookieCount += cookiesThisHour
     var cookieString = `${hoursOfOperation[i]}: ${cookiesThisHour} cookies`
     locations[j].cookiesPerHour.push(cookieString)
   }
-  
+  //console.log(totalSales)
 }
-console.log(Seattle.cookiesPerHour)
-console.log(Lima.cookiesPerHour)
+// console.log(Seattle.cookiesPerHour)
+// console.log(Lima.cookiesPerHour)
 
 
 
 
-document.addEventListener('DOMContentLoaded', (event) => {
-  var cookieList = document.getElementById('enter');
-  cookieList.innerHTMNL += "<li> additional HTML code</li>"
-})
+var mySection = document.getElementById('listsection')
+
+for( var j = 0; j < locations.length; j++){
+  var ulEl = document.createElement('ul');
+  var liEl = document.createElement('p');
+  liEl.innerText = locations[j].name 
+  ulEl.appendChild(liEl)
+  for( var i = 0; i < hoursOfOperation.length; i++ ){
+    liEl = document.createElement('li')
+    liEl.innerText = locations[j].cookiesPerHour[i]
+      ulEl.appendChild(liEl)
+  }
+  liEl = document.createElement('li')
+  liEl.innerText = `Total: ${locations[j].cookieCount} cookies`
+  ulEl.appendChild(liEl)
+  mySection.appendChild(ulEl)
+}
