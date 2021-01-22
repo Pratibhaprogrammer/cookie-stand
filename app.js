c85ce7f43c01bd969c7595ea39863850970a2f46
Jacob Holmer <yacub90@gmail.com>
2021-01-21 18:22:06 -0800

    added form and footer

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict'
-
+var shops = []
 // var Seattle = {
 //   name: 'Seattle',
 //   minCust: 23,
@@ -38,19 +38,21 @@ var tokyo = new Store('Tokyo', 3, 24, 1.2);
 var dubai = new Store('Dubai', 11, 38, 3.7);
 var paris = new Store('Paris', 20, 38, 2.3);
 var lima = new Store('Lima', 2, 16, 5.6);
-
-
-for(var j = 0; j < storeArray.length; j++){
-  
-  for(var i = 0; i < hoursOfOperation.length; i++){
-    var customers = storeArray[j].numOfCustomers();
-    var cookiesThisHour = Math.floor(customers * storeArray[j].cookieSale)
-    storeArray[j].cookieCount += cookiesThisHour
-    var cookieString = cookiesThisHour
-    storeArray[j].cookiesPerHour.push(cookieString)
+ 
+function generateHourlyTotals(){
+  for(var j = 0; j < storeArray.length; j++){
+    
+    for(var i = 0; i < hoursOfOperation.length; i++){
+      var customers = storeArray[j].numOfCustomers();
+      var cookiesThisHour = Math.floor(customers * storeArray[j].cookieSale)
+      storeArray[j].cookieCount += cookiesThisHour
+      var cookieString = cookiesThisHour
+      storeArray[j].cookiesPerHour.push(cookieString)
+    }
+    //console.log(totalSales)
   }
-  //console.log(totalSales)
 }
+generateHourlyTotals()
 // console.log(Seattle.cookiesPerHour)
 // console.log(Lima.cookiesPerHour)
 
@@ -74,15 +76,19 @@ var mySection = document.getElementById('listsection')
 //   ulEl.appendChild(liEl)
 //   mySection.appendChild(ulEl)
 // }
-
+//This is where i built my table
 var table = document.createElement('table')
+table.setAttribute('id','table')
+
+function generateTable(){
+
 var tableBody = document.createElement('tbody')
 var hoursRow = document.createElement('tr')
 tableBody.appendChild(hoursRow)
 table.appendChild(tableBody)
 var blankCell = document.createElement('td')
 hoursRow.appendChild(blankCell)
-
+// this is the loop system i used to list my data to the table
 for( var i =0; i < hoursOfOperation.length; i++){
   var hoursColumn = document.createElement('td')
   hoursColumn.innerText = hoursOfOperation[i]
@@ -102,8 +108,52 @@ for( var j = 0; j < storeArray.length; j++){
     column.innerText = storeArray[j].cookiesPerHour[i]
     row.appendChild(column)
   } 
-  var totalsCell = document.createElement('td')
-  totalsCell.innerText = storeArray[j].cookieCount
-  row.appendChild(totalsCell)
+
+    var totalsCell = document.createElement('td')
+    totalsCell.innerText = storeArray[j].cookieCount
+    row.appendChild(totalsCell)
 }
-mySection.appendChild(table)
+    var footer = document.createElement('tfoot');
+    var footerRow = document.createElement('tr');
+    footer.appendChild(footerRow);
+    var header = document.createElement('th');
+    header.innerText ='totals';
+    footerRow.appendChild(header)
+    for (var i = 0; i < hoursOfOperation.length; i++){
+      var totalCookies = 0
+      for (var j = 0; j < storeArray.length; j++){
+        totalCookies += storeArray[j].cookiesPerHour[i]
+      }
+      var footerData = document.createElement('td');
+      footerData.innerText = totalCookies;
+      footerRow.appendChild(footerData);
+
+    }
+    table.appendChild(footer)
+}
+mySection.appendChild(table);
+
+
+generateTable()
+//takes user input and pushes it into the new constructor (cookieFormCunstructor)
+var inputElement = document.getElementById('cookie-stand')
+inputElement.addEventListener('submit', function(event){
+  event.preventDefault();
+  console.log(event.target.name.value)
+  console.log(event.target.minCust.value);
+  console.log(event.target.maxCust.value);
+  console.log(event.target.cookieSale.value);
+
+  var name = event.target.name.value;
+  var minCust =parseInt(event.target.minCust.value);
+  var maxCust = parseInt(event.target.maxCust.value);
+  var cookieSale =parseInt( event.target.cookieSale.value);
+  var cookieFormConstructor = new Store(name, minCust, maxCust, cookieSale);
+  console.log(storeArray)
+  const destroy = container => {
+    document.getElementById('table').innerHTML = '';
+  };
+  generateHourlyTotals()
+  destroy()
+  generateTable()
+});
